be8ae9238c4f67a4c46a5ab4fbf57d8fb3a8e804
Software-FullStack-IA <wdmsantana@gmail.com>
2023-12-11 17:59:10 -0400

    This months day- boxes

--- src/components/callendar/Callendar.vue
+++ src/components/callendar/Callendar.vue
@@ -1,7 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Day from "./Day.vue";
+import { ref } from "vue";
+
+const todaysFullDate = new Date();
+let lastDayOfThisMonth = new Date(
+  todaysFullDate.getFullYear(),
+  todaysFullDate.getMonth() + 1,
+  0
+);
+
+const daysInThisMonth = () => {
+  let days = [];
+  let lastDayInt: number = lastDayOfThisMonth.getDate();
+  while (lastDayInt) {
+    days.unshift(
+      `${todaysFullDate.getFullYear()}/${
+        todaysFullDate.getMonth() + 1
+      }/${lastDayInt}`
+    );
+    lastDayInt--;
+  }
+  return days;
+};
+
+const dayArr = ref(daysInThisMonth());
+</script>
 
 <template>
-  <div>callendar</div>
+  <div>
+    callendar
+    <div :class="$style.daysContainer">
+      <Day v-for="(day, i) in dayArr" :key="i" :title="day" />
+    </div>
+  </div>
 </template>
 
-<style lang="scss" module scoped></style>
+<style lang="scss" module scoped>
+.daysContainer {
+  gap: 1rem;
+  display: grid;
+}
+</style>
---
+++ src/components/callendar/Day.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div :class="$style.container">{{ props.title }}</div>
+</template>
+
+<style module lang="scss" scoped>
+.container {
+  height: 10rem;
+  width: 10rem;
+  background-color: slategrey;
+  color: white;
+  border-radius: 10%;
+  text-align: center;
+}
+</style>
